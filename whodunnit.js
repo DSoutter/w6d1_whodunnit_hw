20a64a2db8f463e200b30ca22a73a383b6cd4b97
DSoutter <duncan.soutter@gmail.com>
2021-08-23 15:18:23 +0100

    3 done

--- whodunnit.js
+++ whodunnit.js
@@ -1,34 +1,52 @@
 // 1.
 
-const scenario = {
-    murderer: 'Miss Scarlet',
-    room: 'Library',
-    weapon: 'Rope'
-  };
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Library',
+//     weapon: 'Rope'
+//   };
   
-  const declareMurderer = function() {
-    return `The murderer is ${scenario.murderer}.`;
-  }
+//   const declareMurderer = function() {
+//     return `The murderer is ${scenario.murderer}.`;
+//   }
   
-  const verdict = declareMurderer();
-  console.log(verdict);
+//   const verdict = declareMurderer();
+//   console.log(verdict);
 
 //   the return should be "The murderer is Miss Scarlet"
 
 // 2.
 
-const murderer = 'Professor Plum';
+// const murderer = 'Professor Plum';
 
-const changeMurderer = function() {
-  murderer = 'Mrs. Peacock';
-}
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// the return should be error as const murderer already created.
+
+// 3.
+
+let murderer = 'Professor Plum';
 
 const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
   return `The murderer is ${murderer}.`;
 }
 
-changeMurderer();
-const verdict = declareMurderer();
-console.log(verdict);
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
 
-// the return should be error as const murderer already created.
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+
+// the return should be "First verdict: Mrs. Peacock" then "Second Verdict: The murderer is Professor Plum" 
+
